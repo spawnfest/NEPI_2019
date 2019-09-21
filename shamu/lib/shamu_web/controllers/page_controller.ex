72beb4cb2e05ff82722d1f633e1bf510af07bd46
Lukasz Skotarek <l.skotarek@blockfi.com>
2019-09-21 16:24:03 +0200

    generate simple module

--- shamu/lib/shamu_web/controllers/page_controller.ex
+++ shamu/lib/shamu_web/controllers/page_controller.ex
@@ -7,9 +7,44 @@ defmodule ShamuWeb.PageController do
 
   # curl -X POST --data "test" localhost:4000/generate
   # as for now will always respond with the same reply
-  def generate(conn, params) do
-    IO.inspect(params)
+  def generate(conn, %{"data" => data} = _params) do
+    #IO.inspect(params)
 
-    json(conn, %{test: 3, second_test: "test"})
+    #IO.inspect data
+    #IO.inspect x
+
+    x = parse_node(data, [])
+
+    IO.inspect x
+
+    [_, %{name: name, type: "worker"}, _] = x
+
+    d = create_worker(String.to_atom(name))
+
+    s = Macro.to_string(d)
+
+    IO.inspect(d)
+    IO.inspect(s)
+
+    #{:ok, file} = File.open("test2.exs", [:write])
+    #IO.binwrite(file, s)
+    #File.close(file)
+
+    json(conn, s)
+  end
+
+  defp parse_node(%{"name" => name, "type" => type, "children" => []}, acc) do
+    [ %{name: name, type: type} | acc]
+  end
+  defp parse_node(%{"children" => [child | rest]} = node, acc) do
+    parse_node(child, acc) ++ parse_node(%{node | "children" => rest}, acc)
+  end
+
+  defp create_worker(name) do
+    quote do
+      defmodule unquote(name) do
+        def hello(), do: "hello, world!"
+      end
+    end
   end
 end
