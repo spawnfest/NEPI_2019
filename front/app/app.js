e331578b590a5d06de5565e499cf1744e56702cb
Maciej Samoraj <maciej.samoraj@gmail.com>
2019-09-21 21:50:28 +0200

    labels working

--- front/app/app.js
+++ front/app/app.js
@@ -90,7 +90,8 @@ function getData() {
     let element = elements[i];
     if (element instanceof Shape) {
       shapes.push({
-        name: element.businessObject.name,
+        id: element.id,
+        name: element.businessObject.name || element.id,
         children: [],
         type: element.type == "bpmn:UserTask" ? 'supervisor' : 'worker',
         root: true,
@@ -101,8 +102,8 @@ function getData() {
   for (let i = 0; i < elements.length; i++) {
     let element = elements[i];
     if (element instanceof Connection) {
-      let source = shapes.find(s => s.name == element.source.id);
-      let dest = shapes.find(s => s.name == element.target.id);
+      let source = shapes.find(s => s.id == element.source.id);
+      let dest = shapes.find(s => s.id == element.target.id);
       source.children.push(dest);
       dest.root = false;
     }
@@ -112,6 +113,7 @@ function getData() {
 }
 
 window.send = function () {
+  console.log(getData());
   document.querySelector('#code-container').innerHTML = "";
   fetch('http://localhost:4000/generate', {
     method: 'POST',
